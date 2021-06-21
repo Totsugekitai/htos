d2b6250d34b55e0137bfc2e5033143e49f3ea840
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-06-21 17:50:34 +0900

    passed building kernel file

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn entry() {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
 }
