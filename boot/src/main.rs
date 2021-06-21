4980c88b509dc4bb5ecb9ca0f9675c8ce2d9f2cd
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-06-21 12:10:59 +0900

    wip

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -2,11 +2,15 @@
 #![no_main]
 #![feature(abi_efiapi)]
 
+#[macro_use]
+extern crate alloc;
+
 use core::{fmt::Write, panic::PanicInfo};
 use uefi::prelude::*;
 use uefi::proto::console::gop::GraphicsOutput;
 use uefi::proto::media::{fs::SimpleFileSystem, file::*};
 use htlib::boot::BootInfo;
+use uefi::table::boot::MemoryType;
 
 #[entry]
 fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
@@ -40,22 +44,76 @@ fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
     }
 
     // load kernel file
-    load_kernel(&boot_services);
+    let kernel = match load_kernel(&boot_services) {
+        Ok(kernel_addr) => kernel_addr,
+        Err(s) => match s {
+            Status::BUFFER_TOO_SMALL => {
+                writeln!(stdout, "Failed to load kernel: buffer too small");
+                panic!();
+            },
+            Status::NOT_FOUND => {
+                writeln!(stdout, "Failed to load kernel: not found");
+                panic!();
+            },
+            _ => {
+                writeln!(stdout, "Failed to load kernel: unknown reason");
+                panic!();
+            },
+        }
+    };
 
     loop {}
 }
 
 const KERNEL_FILE: &str = "htkernel.elf";
 
-fn load_kernel(bs: &BootServices) {
-    if let Ok(simple_fs) = bs.locate_protocol::<SimpleFileSystem>() {
-        let simple_fs = simple_fs.unwrap();
-        let simple_fs = unsafe { &mut *simple_fs.get() };
+fn load_kernel(bs: &BootServices) -> Result<&'static mut [u8], Status> {
+    let simple_fs = match bs.locate_protocol::<SimpleFileSystem>() {
+        Ok(simple_fs) => simple_fs.unwrap(),
+        Err(e) => return Err(e.status()),
+    };
+    let simple_fs = unsafe { &mut *simple_fs.get() };
 
-        if let Ok(rootdir) = simple_fs.open_volume() {
-            let kernel = rootdir.unwrap().open(KERNEL_FILE, FileMode::Read, FileAttribute::empty());
-        }
-    }
+    let rootdir = match simple_fs.open_volume() {
+        Ok(rootdir) => rootdir.unwrap(),
+        Err(e) => return Err(e.status()),
+    };
+
+    let kernel_handle = match rootdir.open(KERNEL_FILE, FileMode::Read, FileAttribute::empty()) {
+        Ok(kernel_handle) => kernel_handle.unwrap(),
+        Err(e) => return Err(e.status()),
+    };
+
+    let mut kernel_file = match kernel_handle.into_type().unwrap().unwrap() {
+        FileType::Regular(file) => file,
+        FileType::Dir(_) => return Err(Status::ACCESS_DENIED),
+    };
+
+    if let Err(e) = kernel_file.set_position(RegularFile::END_OF_FILE) {
+        return Err(e.status());
+    };
+
+    let kernel_file_size = match kernel_file.get_position() {
+        Ok(size) => size.unwrap(),
+        Err(e) => return Err(e.status()),
+    } as usize;
+
+    if let Err(e) = kernel_file.set_position(0) {
+        return Err(e.status());
+    };
+
+    let pool = match bs.allocate_pool(MemoryType::LOADER_DATA, kernel_file_size) {
+        Ok(pool) => pool.unwrap(),
+        Err(e) => return Err(e.status()),
+    };
+
+    let buf = unsafe { core::slice::from_raw_parts_mut(pool, kernel_file_size) };
+
+    if let Err(e) = kernel_file.read(buf) {
+        return Err(e.status());
+    };
+
+    Ok(buf)
 }
 
 #[panic_handler]
