fb44b7aa797fa366d5268330aa506c07af97dbec
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-06-21 17:49:32 +0900

    fix naming rule

--- lib/src/elf.rs
+++ lib/src/elf.rs
@@ -1,66 +1,87 @@
-#![allow(non_camel_case_types)]
-pub type Elf64_Addr = u64;
-pub type Elf64_Off = u64;
-pub type Elf64_Half = u16;
-pub type Elf64_Word = u32;
-pub type Elf64_Sword = u64;
-pub type Elf64_Xword = u64;
-pub type Elf64_Sxword = u64;
+pub type Elf64Addr = u64;
+pub type Elf64Off = u64;
+pub type Elf64Half = u16;
+pub type Elf64Word = u32;
+pub type Elf64Sword = u64;
+pub type Elf64Xword = u64;
+pub type Elf64Sxword = u64;
 
 #[repr(C)]
-#[derive(Debug, Default)]
-pub struct Elf64_Ehdr {
+#[repr(packed)]
+#[derive(Default)]
+pub struct Elf64Ehdr {
     pub e_ident: [u8; 16],
-    pub e_type: Elf64_Half,
-    pub e_machine: Elf64_Half,
-    pub e_version: Elf64_Word,
-    pub e_entry: Elf64_Addr,
-    pub e_phoff: Elf64_Off,
-    pub e_shoff: Elf64_Off,
-    pub e_flags: Elf64_Word,
-    pub e_ehsize: Elf64_Half,
-    pub e_phentsize: Elf64_Half,
-    pub e_phnum: Elf64_Half,
-    pub e_shentsize: Elf64_Half,
-    pub e_shnum: Elf64_Half,
-    pub e_shstrndx: Elf64_Half,
+    pub e_type: Elf64Half,
+    pub e_machine: Elf64Half,
+    pub e_version: Elf64Word,
+    pub e_entry: Elf64Addr,
+    pub e_phoff: Elf64Off,
+    pub e_shoff: Elf64Off,
+    pub e_flags: Elf64Word,
+    pub e_ehsize: Elf64Half,
+    pub e_phentsize: Elf64Half,
+    pub e_phnum: Elf64Half,
+    pub e_shentsize: Elf64Half,
+    pub e_shnum: Elf64Half,
+    pub e_shstrndx: Elf64Half,
 }
 
-impl Elf64_Ehdr {
+#[derive(Debug)]
+pub enum ElfType {
+    NONE,
+    REL,
+    EXEC,
+    DYN,
+    CORE,
+    UNKNOWN,
+}
+
+impl Elf64Ehdr {
     pub fn is_valid(&self) -> bool {
         const MAGIC: [u8; 4] = *b"\x7fELF";
-        self.e_ident[..3] == MAGIC
+        self.e_ident[..4] == MAGIC
+    }
+
+    pub fn elf_type(&self) -> ElfType {
+        match self.e_type {
+            0 => ElfType::NONE,
+            1 => ElfType::REL,
+            2 => ElfType::EXEC,
+            3 => ElfType::DYN,
+            4 => ElfType::CORE,
+            _ => ElfType::UNKNOWN,
+        }
     }
 }
 
 #[repr(C)]
 #[derive(Debug, Default)]
-pub struct Elf64_Phdr {
-    pub p_type: Elf64_Word,
-    pub p_flags: Elf64_Word,
-    pub p_offset: Elf64_Off,
-    pub p_vaddr: Elf64_Addr,
-    pub p_paddr: Elf64_Addr,
-    pub p_filesz: Elf64_Xword,
-    pub p_memsz: Elf64_Xword,
-    pub p_align: Elf64_Xword,
+pub struct Elf64Phdr {
+    pub p_type: Elf64Word,
+    pub p_flags: Elf64Word,
+    pub p_offset: Elf64Off,
+    pub p_vaddr: Elf64Addr,
+    pub p_paddr: Elf64Addr,
+    pub p_filesz: Elf64Xword,
+    pub p_memsz: Elf64Xword,
+    pub p_align: Elf64Xword,
 }
 
-impl Elf64_Phdr {
+impl Elf64Phdr {
     
 }
 
 #[repr(C)]
 #[derive(Debug, Default)]
-pub struct Elf64_Shdr {
-    pub sh_name: Elf64_Word,
-    pub sh_type: Elf64_Word,
-    pub sh_flags: Elf64_Xword,
-    pub sh_addr: Elf64_Addr,
-    pub sh_offset: Elf64_Off,
-    pub sh_size: Elf64_Xword,
-    pub sh_link: Elf64_Word,
-    pub sh_info: Elf64_Word,
-    pub sh_addralign: Elf64_Xword,
-    pub sh_entsize: Elf64_Xword,
+pub struct Elf64Shdr {
+    pub sh_name: Elf64Word,
+    pub sh_type: Elf64Word,
+    pub sh_flags: Elf64Xword,
+    pub sh_addr: Elf64Addr,
+    pub sh_offset: Elf64Off,
+    pub sh_size: Elf64Xword,
+    pub sh_link: Elf64Word,
+    pub sh_info: Elf64Word,
+    pub sh_addralign: Elf64Xword,
+    pub sh_entsize: Elf64Xword,
 }
