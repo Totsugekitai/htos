cf6887e356953cd1cb92ba01a6fb3d610f3ff33e
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-06-22 11:42:05 +0900

    define error type

---
+++ lib/src/error.rs
@@ -0,0 +1,18 @@
+pub struct Error {
+    pub kind: ErrorKind,
+}
+
+impl Default for Error {
+    fn default() -> Self {
+        Error { kind: ErrorKind::NotSupported }
+    }
+}
+
+#[non_exhaustive]
+pub enum ErrorKind {
+    NotFound,
+    Empty,
+    PermissionDenied,
+    InvalidParameter,
+    NotSupported,
+}
