4a7b9a8d1e30074be04a37a9d5511b4de86bdae9
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-06-26 15:44:13 +0900

    wip

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,10 +5,11 @@ use core::panic::PanicInfo;
 use htlib::boot::*;
 use htkernel::*;
 
+
 #[no_mangle]
 extern "C" fn kernel_entry(bi: &BootInfo) {
     let mut fb: graphics::FrameBuffer = Default::default();
-    fb.init(bi);
+    fb.set(fb.init(bi));
 
     fb.write_background(htkernel::graphics::Color::Black);
 
