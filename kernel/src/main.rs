ec2607c3624f98c67112e5e015af284797d8406a
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-07-03 17:33:33 +0900

    fix

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -10,10 +10,6 @@ extern "C" fn kernel_entry(boot_info: &BootInfo) {
     htkernel::init(boot_info);
     println!("Hello, HTOS!");
 
-    unsafe {
-        core::ptr::write_volatile(0xdeadbeef as *mut u64, 42);
-    };
-
     loop {}
 }
 
