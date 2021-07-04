449ed00604f9de6165da662f4157a720ae5556a9
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-07-04 21:04:26 +0900

    reformat

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -3,12 +3,12 @@
 #![feature(abi_efiapi)]
 
 use core::{fmt::Write, panic::PanicInfo};
-use uefi::Status;
+use htlib::{boot::*, elf::*, error::*};
 use uefi::prelude::*;
 use uefi::proto::console::{gop::*, text::Output};
-use uefi::proto::media::{fs::SimpleFileSystem, file::*};
-use uefi::table::boot::{MemoryType, MemoryDescriptor};
-use htlib::{boot::*, elf::*, error::*};
+use uefi::proto::media::{file::*, fs::SimpleFileSystem};
+use uefi::table::boot::{MemoryDescriptor, MemoryType};
+use uefi::Status;
 
 #[entry]
 fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
@@ -25,7 +25,7 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
         let gop = gop.unwrap();
         let gop = unsafe { &mut *gop.get() };
 
-        boot_info.vram_base = gop.frame_buffer().as_mut_ptr() as usize as u64;
+        boot_info.vram_base = gop.frame_buffer().as_mut_ptr() as u64;
 
         let mode_info = gop.current_mode_info();
 
@@ -49,12 +49,16 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
     }
 
     // test frame buffer
-    let vram_base = boot_info.vram_base;
-    for i in 0..(boot_info.vram_width as u64 * boot_info.vram_height as u64) {
-        let vram = (vram_base + i * 4) as *mut Pixel;
-        let pixel = Pixel { dot: (0x50500000 as u32).to_be() };
-        unsafe { core::ptr::write_volatile::<Pixel>(vram, pixel); }
-    }
+    //let vram_base = boot_info.vram_base;
+    //for i in 0..(boot_info.vram_width as u64 * boot_info.vram_height as u64) {
+    //    let vram = (vram_base + i * 4) as *mut Pixel;
+    //    let pixel = Pixel {
+    //        dot: (0x50500000 as u32).to_be(),
+    //    };
+    //    unsafe {
+    //        core::ptr::write_volatile::<Pixel>(vram, pixel);
+    //    }
+    //}
 
     // load kernel file
     let kernel_addr = match load_kernel(&bs) {
@@ -63,19 +67,19 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
             Status::BUFFER_TOO_SMALL => {
                 writeln!(stdout, "Failed to load kernel: buffer too small").unwrap();
                 panic!();
-            },
+            }
             Status::NOT_FOUND => {
                 writeln!(stdout, "Failed to load kernel: not found").unwrap();
                 panic!();
-            },
+            }
             _ => {
                 writeln!(stdout, "Failed to load kernel: unknown reason").unwrap();
                 panic!();
-            },
-        }
+            }
+        },
     };
 
-    let kernel_ehdr = unsafe { &*(kernel_addr as *const [u8] as *const Elf64Ehdr) };
+    let kernel_ehdr = unsafe { &*(kernel_addr as *const _ as *const Elf64Ehdr) };
 
     if kernel_ehdr.is_valid() {
         writeln!(stdout, "Valid ELF file").unwrap();
@@ -102,13 +106,20 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
         core::mem::transmute::<u64, extern "sysv64" fn(bi: &BootInfo)>(kernel_ehdr.e_entry)
     };
 
-    writeln!(stdout, "elf file addr: 0x{:x}", kernel_addr as *const [u8] as *const u8 as usize).unwrap();
+    writeln!(
+        stdout,
+        "elf file addr: 0x{:x}",
+        kernel_addr as *const [u8] as *const u8 as usize
+    )
+    .unwrap();
     writeln!(stdout, "kernel entry: 0x{:x}", kernel_entry as usize).unwrap();
 
     // for debug
     for i in 0..100 {
         let a = kernel_entry as u64;
-        unsafe { write!(stdout, "{:x} ", *((a + i) as *const u8)).unwrap(); }
+        unsafe {
+            write!(stdout, "{:x} ", *((a + i) as *const u8)).unwrap();
+        }
     }
 
     // exit boot services
@@ -118,10 +129,11 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
         Err(_s) => {
             writeln!(stdout, "Memory allocate error").unwrap();
             panic!();
-        },
+        }
     };
     let mut mmap_buf = unsafe { core::slice::from_raw_parts_mut(mmap_buf, mmap_size) };
-    let (_st, _iter) = st.exit_boot_services(handle, &mut mmap_buf)
+    let (_st, _iter) = st
+        .exit_boot_services(handle, &mut mmap_buf)
         .expect_success("Failed to boot services");
 
     // jump!!!
@@ -181,10 +193,10 @@ fn load_kernel(bs: &BootServices) -> Result<&'static [u8], Status> {
 }
 
 fn allocate(bs: &BootServices, size: usize) -> Result<*mut u8, Status> {
-     match bs.allocate_pool(MemoryType::LOADER_DATA, size) {
+    match bs.allocate_pool(MemoryType::LOADER_DATA, size) {
         Ok(pool) => Ok(pool.unwrap()),
         Err(e) => Err(e.status()),
-     }
+    }
 }
 
 pub fn load_segments(head: &[u8], so: &mut Output) -> Result<(), Error> {
@@ -192,12 +204,15 @@ pub fn load_segments(head: &[u8], so: &mut Output) -> Result<(), Error> {
     for i in 0..ehdr.e_phnum as u64 {
         let phdr_offset = ehdr.e_phoff + (ehdr.e_phentsize as u64) * i;
         unsafe {
-            let phdr = &*((head as *const [u8] as *const u8 as u64 + phdr_offset) as *const Elf64Phdr);
+            let phdr =
+                &*((head as *const [u8] as *const u8 as u64 + phdr_offset) as *const Elf64Phdr);
             if let Err(e) = phdr.load_segmemt(head) {
                 use htlib::error::ErrorKind::*;
                 match e.kind {
-                    NotFound => { writeln!(so, "This segment is not LOAD").unwrap() },
-                    _ => { return Err(e); }
+                    NotFound => writeln!(so, "This segment is not LOAD").unwrap(),
+                    _ => {
+                        return Err(e);
+                    }
                 }
             } else {
                 writeln!(so, "Load Segment").unwrap();
@@ -206,7 +221,6 @@ pub fn load_segments(head: &[u8], so: &mut Output) -> Result<(), Error> {
     }
     Ok(())
 }
-
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
